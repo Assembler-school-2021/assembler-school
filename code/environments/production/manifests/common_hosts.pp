2544922f29061681efb8b77605f0507edc1cc4b4
root <root@puppet-master.local>
2021-06-03 21:22:40 +0200

    Cabios de ip y de location

--- code/environments/production/manifests/common_hosts.pp
+++ code/environments/production/manifests/common_hosts.pp
@@ -5,6 +5,6 @@ class common_hosts {
 
   # añade algunas entradas a hosts
   host { 'localhost':             ip => '127.0.0.1', }
-  host { 'puppet-master.local':   ip => '65.21.188.73', host_aliases => [ 'puppet-master', 'puppet' ] }
-  host { 'puppet-agent.local':    ip => '95.216.219.230', host_aliases => [ 'puppet-agent' ] }
+  host { 'puppet-master.local':   ip => '95.216.219.70', host_aliases => [ 'puppet-master', 'puppet' ] }
+  host { 'puppet-agent.local':    ip => '95.216.215.240', host_aliases => [ 'puppet-agent' ] }
 }
